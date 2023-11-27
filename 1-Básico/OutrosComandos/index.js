0742d719005ef6a5f02ccdd13deb9c02bd4ac8f6
Daniel Duarte <dev.daniel.duarte@gmail.com>
2023-11-27 12:08:31 -0300

    adicionando outros comandos: alert,confirm e prompt

---
+++ 1-Básico/OutrosComandos/index.js
@@ -0,0 +1,13 @@
+alert("Olá, Mundo!!!!!!"); //não posso executar no node 
+
+window.alert("Usando a classe window"); 
+let confirmacao = window.confirm("Deseja sair?"); //retorna um valor boolean 
+console.log(confirmacao); 
+
+
+let num1 = Number(window.prompt('Digite o número 1: ')); 
+let num2 = Number(window.prompt("Digite o número 2: ")); 
+window.alert(`A soma de ${num1} + ${num2} = ${num1 + num2}`); 
+
+
+
