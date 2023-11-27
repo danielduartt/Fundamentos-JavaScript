6f0e2964b42c8c617384906db958d0416039e57d
Daniel Duarte <dev.daniel.duarte@gmail.com>
2023-11-27 08:49:25 -0300

    add arquivos iniciais

---
+++ 1-Básico/ComandoLog.js
@@ -0,0 +1,8 @@
+console.log("Teste"); // String
+console.log('Quebrou a linha');
+console.log(`Posso abrir desse jeito`);
+console.log(10, 100000 , 12323);  // aki nãoq uebra a linha 
+
+
+// Exercício 
+console.log('Meu nome é "DANIEL".', 'Estou aprendendo JavaScript às 8:20 da manhã. '); 
---
+++ 1-Básico/IntegrandoHtml/index.js
@@ -0,0 +1,5 @@
+console.log("olá, Mundo");
+console.log("Esse trecho será exibido no console do navegador! ")
+//alert("Olá mundo!")
+//alert("Enchendo o saco com o aler kk! ") Esse alert não existe no node 
+// mas no navegador ele será executado 
