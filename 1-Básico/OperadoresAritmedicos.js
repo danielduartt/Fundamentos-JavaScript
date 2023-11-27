e14cd21474cfdcfd0a1b0e1402977f42fce2b65a
Daniel Duarte <dev.daniel.duarte@gmail.com>
2023-11-27 09:56:28 -0300

    Adicionando arquivo sobre tipos primitivos e operadores

---
+++ 1-Básico/OperadoresAritmedicos.js
@@ -0,0 +1,26 @@
+/*
+#Operadores Aritmédicos: 
+    +, - , / , * 
+    ** - potenciação
+    % - resto da divisão 
+#Ordem de precedencia
+    ()
+    **
+    * / % 
+    + - 
+#Incrementos e Decrementos 
+    x++ pós-incremento
+    x-- 
+    ou
+    ++x 
+    --x pré-incremento
+#Operadores de atribuição  
+    += 
+    -=
+    *=
+    /= 
+    **= 
+
+*/
+let num1 = 10; 
+let num2 = parseInt('5'); // passar a string para um número e retornar um valor numérico
